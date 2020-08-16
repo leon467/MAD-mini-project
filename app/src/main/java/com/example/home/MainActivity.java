5410c4e29a57f07dbdf54c6b1f16f44f0e0100ba
leoni <leonisaac99@hotmail.com>
2020-08-17 00:08:18 +0530

    Home layout

---
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.home;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3BA1B7")));
+
+    }
+}
