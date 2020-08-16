f3e6febf05f926685ae1c176c18193f9ac055ff3
leoni <leonisaac99@hotmail.com>
2020-08-17 01:26:02 +0530

    Changes made

---
+++ app/src/androidTest/java/cart/MainActivity.java
@@ -0,0 +1,23 @@
+package cart;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3BA1B7")));
+
+    }
+}
